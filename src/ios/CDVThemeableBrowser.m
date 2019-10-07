0e9abab8fdaecae4a2903e18717c2fa2ffc57c31
Torgny Walin <torgny@appfabrikken.no>
2019-10-07 19:36:36 +0200

    Update CDVThemeableBrowser.m
    Test fullscreen

--- src/ios/CDVThemeableBrowser.m
+++ src/ios/CDVThemeableBrowser.m
@@ -255,7 +255,7 @@
         // [self.themeableBrowserViewController setCloseButtonTitle:browserOptions.closebuttoncaption];
     }
     // Set Presentation Style
-    UIModalPresentationStyle presentationStyle = UIModalPresentationFullScreen; // default
+    UIModalPresentationStyle presentationStyle = .fullscreen; // default
     if (browserOptions.presentationstyle != nil) {
         if ([[browserOptions.presentationstyle lowercaseString] isEqualToString:@"pagesheet"]) {
             presentationStyle = UIModalPresentationPageSheet;
